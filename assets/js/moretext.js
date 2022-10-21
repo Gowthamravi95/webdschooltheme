c125a46ee683b8059162e876cc7f59c4f7e1612e
gowthamravi95 <gowthamhawks@gmail.com>
2022-10-21 17:23:22 +0530

    dd

---
+++ assets/js/moretext.js
@@ -0,0 +1,33 @@
+
+  $(document).ready(function(){
+   var moretext = `Read More<i class="fa-solid fa-angle-down"></i>`;
+   var lesstext = `Read Less<i class="fa-solid fa-angle-up"></i>`;
+   $('.readmore').each(function () {
+
+
+      let h = $(this).height();
+      if (h == 230 || h==330) {
+         $(this).after('<div><a href="" class="morelink">' + moretext + '</a></div>');
+
+      } else {
+         $(this).addClass('nobefore');
+      }
+
+   });
+
+   $(".morelink").click(function () {
+      if ($(this).hasClass("less")) {
+         $(this).removeClass("less");
+         $(this).html(moretext);
+
+      } else {
+         $(this).addClass("less");
+         $(this).html(lesstext);
+
+      }
+      $(this).parent().prev().toggleClass('moreStyle nobefore');
+      return false;
+   });
+
+   
+})
