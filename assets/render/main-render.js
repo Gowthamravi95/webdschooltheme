1e93de8daea49144d3a71b2f6163e28e137644e8
gowthamravi95 <gowthamhawks@gmail.com>
2022-10-21 14:33:03 +0530

    dsf

--- assets/render/main-render.js
+++ assets/render/main-render.js
@@ -33,3 +33,39 @@ $(".script-links").append(
    
    `
 )
+
+$(".script-links-online").append(
+    ` 
+    <script src="../assets/render/head-links.js"></script>
+
+    <script src="../assets/render/header.js"></script>
+
+
+
+
+    <script src="../assets/js/vendor/waypoints.js"></script>
+    <script src="../assets/js/bootstrap-bundle.js"></script>
+    <script src="../assets/js/meanmenu.js"></script>
+    <script src="../assets/js/swiper-bundle.js"></script>
+    <script src="../assets/js/owl-carousel.js"></script>
+    <script src="../assets/js/magnific-popup.js"></script>
+    <script src="../assets/js/parallax.js"></script>
+    <script src="../assets/js/backtotop.js"></script>
+    <script src="../assets/js/nice-select.js"></script>
+    <script src="../assets/js/counterup.js"></script>
+    <script src="../assets/js/wow.js"></script>
+    <script src="../assets/js/isotope-pkgd.js"></script>
+    <script src="../assets/js/imagesloaded-pkgd.js"></script>
+    <script src="../assets/js/ajax-form.js"></script>
+    <script src="../assets/js/slick.min.js"></script>
+ 
+    <script src="../assets/js/main.js"></script>
+    <script src="../assets/js/client.js"></script>
+ 
+
+   
+
+   
+   
+   `
+)
