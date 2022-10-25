dc457d4b3b4f6b7cbb995e79ea9f29017cde17b9
Gowthamravi95 <gowthamhawks@gmail.com>
2022-10-25 15:53:43 +0530

    dsf

--- assets/js/main.js
+++ assets/js/main.js
@@ -426,54 +426,9 @@
     });
 
 
-
-	$(document).ready(function(){
-		$(".tp-btn-green").click(function(){
-		   alert("dzsfkj");
-	
-		})
-		
-		var moretext = `Read More<i class="fa-solid fa-angle-down"></i>`;
-		var lesstext = `Read Less<i class="fa-solid fa-angle-up"></i>`;
-		$('.more').each(function () {
-	
-	
-		   let h = $(this).height();
-	
-		   if (h == 120) {
-			  $(this).after('<div><a href="" class="morelink">' + moretext + '</a></div>');
-	
-		   } else {
-			  $(this).addClass('nobefore');
-		   }
-	
-		});
-	
-		$(".morelink").click(function () {
-		   if ($(this).hasClass("less")) {
-			  $(this).removeClass("less");
-			  $(this).html(moretext);
-	
-		   } else {
-			  $(this).addClass("less");
-			  $(this).html(lesstext);
-	
-		   }
-		   $(this).parent().prev().toggleClass('moreStyle nobefore');
-		   return false;
-		});
-	
-		
-	 })
-
-
-
-
-
-
-
-
-
+////////////////////////////////////////////////////
+	// 23. Catergory sticky top
+	$(".course-tab").css({"position":"sticky","top":"40px","background-color":"#F7F7F7","padding-top":"20px","z-index":"9"})
 
 
 
