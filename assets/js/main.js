12de772f42bc84f1272084db1c323ac1e3dddaf0
gowthamravi95 <gowthamhawks@gmail.com>
2022-10-28 17:59:25 +0530

    ASDF

--- assets/js/main.js
+++ assets/js/main.js
@@ -437,4 +437,23 @@
 
 
 
+
+// setTimout is not necessary
+setTimeout(function() {
+  
+	$('.recaptcha').each(function() {
+	  grecaptcha.render(this.id, {
+		'sitekey': '6LdVkwkUAAAAACeeETRX--v9Js0vWyjQOTIZxxeB',
+		"theme":"light"
+	  });
+	});
+	
+  }, 2000);
+
+
+
+
+
+
+
 })(jQuery);
